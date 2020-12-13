8a39289ed9e6427da6cd76b3cb23144e95d6b3d6
Salah Osman <osman144@umn.edu>
2020-12-13 17:41:58 -0600

    Fix footer

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -9,13 +9,13 @@ class Footer extends React.Component {
                 <div className="footer_inner">
                     <div className="container">
                       <div className="row">
-                        <div className="col">
+                        <div className="col-md-4 mb-4 ">
                           <h3>Find us:</h3>
                           <address style={{margin: 0}} className="map">Minneapolis, MN</address>
                           <address style={{margin: 0}} className="mail"><a href="mailto:kookeology@gmail.com">kookeology@gmail.com</a></address>
                           <address style={{margin: 0}} className="phone">(990) 000 0000</address>
                         </div>
-                        <div className="col">
+                        <div className="col-md-4 mb-4">
                           <h3>Follow us</h3>
                           <ul className="social">
                             <li><a className="facebook" href="https://facebook.com/kookeology">Facebook</a></li>
@@ -23,8 +23,8 @@ class Footer extends React.Component {
                             <li><a className="pinterest" href="https://pinterest.com/kookeology/">Pinterest</a></li>
                           </ul>
                         </div>
-                        <div className="col">
-                          <h3>News Letter</h3>
+                        <div className="col-md-4 mb-4">
+                          <h3>Shoot us an email</h3>
                           <p className="form-newsletter">Contact us: Kookeology@gmail.com</p>
                           <p>Contact us and ask us a question or leave a comment.</p>
                         </div>
